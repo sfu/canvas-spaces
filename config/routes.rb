d24609502ac11669155687659dd44e6f29c26b90
Patrick Chin <patchin@sfu.ca>
2015-03-11 10:39:12 -0700

    Added constraints. Added comments. Deleted some test routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
-  # authenticate using this route
+  # Authenticate in the browser using this route.
+  # Will take you to the CAS login page if you don't have the right auth cookies.
   get '/canvasspaces/login', to: 'Manager#login'
   
-  # canvas looks for '/api/v' to determine if this is an api request.
-  # we could override the bool method for determining if the route is an api call or not.
+  # Canvas looks for '/api/v' to determine if this is an api request.
+  # We could override the bool method for determining if the route is an api call or not.
   get '/api/v1/canvasspaces/groups', to: 'Manager#list_groups'
   post '/api/v1/canvasspaces/groups', to: 'Manager#create_group'
-  get '/api/v1/canvasspaces/groups/:group_id', to: 'Manager#group_info'
-  put '/api/v1/canvasspaces/groups/:group_id', to: 'Manager#modify_group'
-  get '/api/v1/canvasspaces/groups/:group_id/users', to: 'Manager#list_users'
-  post '/api/v1/canvasspaces/groups/:group_id/users', to: 'Manager#add_user'
-  delete '/api/v1/canvasspaces/groups/:group_id/users/:user_id', to: 'Manager#remove_user'  #, :constraints => { :user => /[^\/]+/ }
-  put '/api/v1/canvasspaces/groups/:group_id/leader', to: 'Manager#set_leader'
+  
+  get '/api/v1/canvasspaces/groups/:group_id', to: 'Manager#group_info', constraints: { group_id: /\d+/ }
+  put '/api/v1/canvasspaces/groups/:group_id', to: 'Manager#modify_group', constraints: { group_id: /\d+/ }
+  
+  # Using constraints solves the problem with missing :group_id.
+  # /api/v1/canvasspaces/groups/:group_id/users becomes /api/v1/canvasspaces/groups//users
+  # which matches route for group_info when not using constraints.
+  get '/api/v1/canvasspaces/groups/:group_id/users', to: 'Manager#list_users', constraints: { group_id: /\d+/ }
+
+  post '/api/v1/canvasspaces/groups/:group_id/users', to: 'Manager#add_user', constraints: { group_id: /\d+/ }
+  delete '/api/v1/canvasspaces/groups/:group_id/users/:user_id', to: 'Manager#remove_user', constraints: { group_id: /\d+/, user_id: /\d+/ }
+  
+  put '/api/v1/canvasspaces/groups/:group_id/leader', to: 'Manager#set_leader', constraints: { group_id: /\d+/ }
+  
   # test, test
   get '/api/v1/canvasspaces/test/users', to: 'Manager#test_get_user_list' 
-  post '/api/v1/canvasspaces/test', to: 'Manager#test'
-  get '/api/v1/canvasspaces/test', to: 'Manager#get_test'
 end
