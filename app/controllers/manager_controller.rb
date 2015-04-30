11365df53ecc24fc1281451098b16290fa1f923f
Graham Ballantyne <grahamb@sfu.ca>
2015-04-30 15:07:38 -0700

    check for valid maillists on form submit
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -116,6 +116,17 @@ class ManagerController < ApplicationController
       user
     end
     members.uniq!
+
+    # all maillists must be valid
+    maillists_param.each do | maillist |
+      if (!maillist_is_valid? maillist)
+        render json: { field: 'maillists', error: "\"#{maillist}\" is not a valid maillist" }, status: :bad_request
+        return
+      end
+    end
+    maillists_param.uniq!
+
+
     if @current_user.account.site_admin?
       if leader_id_param && !leader_id_param.blank?
         leader = User.find_by_id(leader_id_param)
