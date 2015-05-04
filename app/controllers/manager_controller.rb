58bb3c5d0d62c5785f5431fcae4ab90dd5e318ab
Graham Ballantyne <grahamb@sfu.ca>
2015-05-04 11:25:41 -0700

    fix pagination url for list_groups_for_user
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -67,7 +67,7 @@ class ManagerController < ApplicationController
       return
     end
     groups = User.find(params[:user_id]).groups.where(group_category_id: GROUP_CATEGORY.id)
-    groups_json = Api.paginate(groups, self, api_v1_canvasspaces_groups_url).map do |g|
+    groups_json = Api.paginate(groups, self, api_v1_canvasspaces_user_groups_url).map do |g|
       g.as_json(only: [:id, :name, :leader_id, :created_at, :description], include_root: false).merge({ member_count: g.users.count, join_type: display_join_type(g.join_level) })
     end
     render :json => groups_json
