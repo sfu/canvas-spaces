48808ebf00d3a9f0c8d7fe3197beb830b2bbec4e
Graham Ballantyne <grahamb@sfu.ca>
2015-05-11 16:11:36 -0700

    only return available groups
    Signed-off-by: Graham Ballantyne <grahamb@sfu.ca>

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -55,7 +55,7 @@ class ManagerController < ApplicationController
       return
     end
 
-    groups = User.find(user_id).groups.where(group_category_id: GROUP_CATEGORY.id).order(:name)
+    groups = User.find(user_id).groups.where(group_category_id: GROUP_CATEGORY.id, workflow_state: 'available').order(:name)
     groups_json = Api.paginate(groups, self, api_v1_canvasspaces_user_groups_url).map do |g|
       g.as_json(only: [:id, :name, :created_at, :description], include_root: false).merge({
         member_count: g.users.count,
