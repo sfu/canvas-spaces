adda1d4cbf6780f1a4e9a7694540b5674e8abf38
Graham Ballantyne <grahamb@sfu.ca>
2015-07-24 16:06:53 -0700

    register plugin and provide settings interface

--- lib/canvas_spaces/engine.rb
+++ lib/canvas_spaces/engine.rb
@@ -1,4 +1,17 @@
 module CanvasSpaces
   class Engine < ::Rails::Engine
+    initializer "canvas_spaces.canvas_plugin" do
+      Canvas::Plugin.register :canvas_spaces, nil, {
+        :name => 'Canvas Spaces',
+        :description => '',
+        :version => CanvasSpaces::VERSION,
+        :settings_partial => 'canvas_spaces/plugin_settings',
+        :hide_from_users => true,
+        :settings => {
+          :groupset_name => 'Canvas Spaces',
+          :public_spaces_enabled => false
+        }
+      }
+    end
   end
 end
